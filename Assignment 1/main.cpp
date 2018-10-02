3e88bc81fa42b448efbeae96c7774936bcd828fc
bader-waseem <baderwaseem@hotmail.com>
2018-10-02 17:35:12 -0600

    Updated main.cpp, finally getting HTTP header info!

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -19,26 +19,39 @@ int main(int argc, char * const argv[]) {
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address = initSocketAddr(AF_INET, 27015, INADDR_ANY); // Initializing socket address
-    int socket1 = createSocket(AF_INET, SOCK_STREAM, 0); // Initalizing socket
+    int listentingSocket = createSocket(AF_INET, SOCK_STREAM, 0); // Initalizing socket
     int status; // This willbe used to determine the status of the socket functions
 
-    status = bind(socket1, (struct sockaddr*)&address, sizeof(struct sockaddr_in)); // Binding socket to address
+    status = bind(listentingSocket, (struct sockaddr*)&address, sizeof(struct sockaddr_in)); // Binding socket to address
     if (status == -1) {
         printf("bind() call failed.");
+        return 1;
     }
 
-    status = listen(socket1, 5);    // Listening to the socket, setting queue length of 5
+    status = listen(listentingSocket, 5);    // Listening to the socket, setting queue length of 5
     if (status == -1) {
         printf("listen() call failed.");
+        return 1;
     }
 
-    int socket2 = accept(socket1, NULL, NULL); // Attempting to accept the connection from a web browser
-    if (socket2 == -1) {
+    int dataSocket = accept(listentingSocket, NULL, NULL); // Attempting to accept the connection from a web browser
+    if (dataSocket == -1) {
         printf("Failed to accept a connection.");
+        return 1;
     }
     else {
         printf("Connection succussful!\n");
     }
+
+    // Declaring variables to be used with the snd() and rcv() functions
+    char sndMessage[4096];
+    char recvMessage[4096 + 1];
+    int count;
+
+    // Going to receive data from the client
+    count = recv(dataSocket, recvMessage, 4096, 0);
+    recvMessage[count] = '\0'; // Null terminating the array
+    printf("Data: \n%s\n", recvMessage);
 
     printf("main reached the end\n");
     return 0;
