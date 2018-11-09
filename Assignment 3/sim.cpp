3d901ac0687ed6427207d627a33b7514354fb58c
bader-waseem <baderwaseem@hotmail.com>
2018-11-09 15:17:05 -0700

    Model is kind of complete
    Detecting packet loss had been a complete failure though. I cann't believe I'm doing so horrible on these assignments, I thought I would have been better by now, but nope it's still all garbage.

--- Assignment 3/sim.cpp
+++ Assignment 3/sim.cpp
@@ -1,7 +1,7 @@
 // Author: Bader Abdulwaseem
 // UCID: 30023849
 // Assignment 3
-// Compilation instructions:
+// Compilation instructions: g++ sim.cpp -o sim.out
 
 #include <iostream>
 #include <fstream>
@@ -38,21 +38,22 @@ struct qosData {
     double delay = 0.0;
     int packetsLost = 0;
     double packetLoss;
-		double averageDelay;
+	double averageDelay;
 };
 
 int main(int argc, char * argv[]) {
     // This queue will contain the arrival times of the packets and their sizes
     queue<vector<double>> buffer;
-	int bufferSize = BUFFER_SIZE_DEFAULT;
-	qosData qos;
+	int maxBufferSize = BUFFER_SIZE_DEFAULT;
+	qosData qos11Mbps, qos6Mbps, qos54Mbps, qos30Mbps;
     // double arrivalTime = START;
     // Delay = arrival time of current packet - previous packet's transmission time
     double delayTime = 0.0;
     // Transmission time = packet size / transmission speed
     double departureTime = START;
 	double currentTime = START;
-	// The transmission speed is in MBps, just so I dont have to multiple the
+	double previousPacketTime = START;
+	// The transmission speed is in Bps, just so I dont have to multiple the
 	// packet size by 8 everytime
     double transmissionSpeed = 1375000.0;
     // double packetSize;
@@ -66,7 +67,6 @@ int main(int argc, char * argv[]) {
     ifstream inputFile;
     inputFile.open("soccer.txt");
 
-	int pushCount = 0;
     string line;
     if (inputFile.is_open()) {
         while (getline(inputFile, line)) {
@@ -75,19 +75,22 @@ int main(int argc, char * argv[]) {
 			pairDouble.push_back(stod(pairStr[0]));
 			pairDouble.push_back(stod(pairStr[1]));
 
-
 			// If a packet arrives before the current one is finished transmitting
 			// add it to the queue
 			if (pairDouble[0] < currentTime) {
-				buffer.push(pairDouble);
 				delayTime = currentTime - pairDouble[0];
+				qos11Mbps.delay += delayTime;
 			}
 
-			if (buffer.size() == bufferSize) {
+			// When the buffer is full, it drops the packet and icrements the
+			// dropped packets counter
+			if (buffer.size() == maxBufferSize) {
 				// Drop packet
-				qos.packetsLost++;
+				qos11Mbps.packetsLost++;
 			}
 
+			// When the buffer's size is 0, there will be no queuing delay as
+			// it is being transmitted immediately
 			else if (buffer.size() == 0) {
 				buffer.push(pairDouble);
 				delayTime = 0.0;
@@ -96,11 +99,12 @@ int main(int argc, char * argv[]) {
 				buffer.pop();
 			}
 
+			// This case checks to see if the buffer is occupied by packets
+			// and applies a queuing delay when the packet is finally transmitted
 			else if (buffer.size() > 0 || buffer.size() < 100) {
 				buffer.push(pairDouble);
 				departureTime = (buffer.front()[1] / transmissionSpeed) + delayTime;
 				currentTime += departureTime;
-				qos.delay += delayTime;
 				buffer.pop();
 			}
 
@@ -112,14 +116,207 @@ int main(int argc, char * argv[]) {
 
     else {
         cerr << "File failed to open." << endl;
+		return -1;
     }
 
     inputFile.close();
 
-	qos.packetLoss = (double)qos.packetsLost / (double)packets * 100.0;
-	qos.averageDelay = qos.delay / packets;
-	cout << "Packet loss: " << qos.packetLoss << "%" << endl;
-	cout << "Average delay: " << qos.averageDelay << endl;
-	cout << pushCount << endl;
+	qos11Mbps.packetLoss = ((double)qos11Mbps.packetsLost / (double)packets) * 100.0;
+	qos11Mbps.averageDelay = qos11Mbps.delay / (double)packets;
+
+	// 6 Mbps in bytes per second
+	transmissionSpeed = 750000.0;
+	inputFile.open("soccer.txt");
+	packets = 0;
+	currentTime = START;
+
+    if (inputFile.is_open()) {
+        while (getline(inputFile, line)) {
+            packets++;   // Incrementing to count total number of packets
+            pairStr = stringSplit(line, ' ');
+			pairDouble.push_back(stod(pairStr[0]));
+			pairDouble.push_back(stod(pairStr[1]));
+
+			// If a packet arrives before the current one is finished transmitting
+			// add it to the queue
+			if (pairDouble[0] < currentTime) {
+				delayTime = currentTime - pairDouble[0];
+				qos6Mbps.delay += delayTime;
+			}
+
+			// When the buffer is full, it drops the packet and icrements the
+			// dropped packets counter
+			if (buffer.size() == maxBufferSize) {
+				// Drop packet
+				qos6Mbps.packetsLost++;
+			}
+
+			// When the buffer's size is 0, there will be no queuing delay as
+			// it is being transmitted immediately
+			else if (buffer.size() == 0) {
+				buffer.push(pairDouble);
+				delayTime = 0.0;
+				departureTime = buffer.front()[1] / transmissionSpeed;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// This case checks to see if the buffer is occupied by packets
+			// and applies a queuing delay when the packet is finally transmitted
+			else if (buffer.size() > 0 || buffer.size() < 100) {
+				buffer.push(pairDouble);
+				departureTime = (buffer.front()[1] / transmissionSpeed) + delayTime;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// Clearing the pair
+			pairDouble.pop_back();
+			pairDouble.pop_back();
+		}
+    }
+
+    else {
+        cerr << "File failed to open." << endl;
+		return -1;
+    }
+
+    inputFile.close();
+
+	qos6Mbps.packetLoss = ((double)qos6Mbps.packetsLost / (double)packets) * 100.0;
+	qos6Mbps.averageDelay = qos6Mbps.delay / (double)packets;
+
+
+	transmissionSpeed = 6750000.0;
+	packets = 0;
+	currentTime = START;
+	inputFile.open("soccer.txt");
+
+    if (inputFile.is_open()) {
+        while (getline(inputFile, line)) {
+            packets++;   // Incrementing to count total number of packets
+            pairStr = stringSplit(line, ' ');
+			pairDouble.push_back(stod(pairStr[0]));
+			pairDouble.push_back(stod(pairStr[1]));
+
+			// If a packet arrives before the current one is finished transmitting
+			// add it to the queue
+			if (pairDouble[0] < currentTime) {
+				delayTime = currentTime - pairDouble[0];
+				qos54Mbps.delay += delayTime;
+			}
+
+			// When the buffer is full, it drops the packet and icrements the
+			// dropped packets counter
+			if (buffer.size() == maxBufferSize) {
+				// Drop packet
+				qos54Mbps.packetsLost++;
+			}
+
+			// When the buffer's size is 0, there will be no queuing delay as
+			// it is being transmitted immediately
+			else if (buffer.size() == 0) {
+				buffer.push(pairDouble);
+				delayTime = 0.0;
+				departureTime = buffer.front()[1] / transmissionSpeed;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// This case checks to see if the buffer is occupied by packets
+			// and applies a queuing delay when the packet is finally transmitted
+			else if (buffer.size() > 0 || buffer.size() < 100) {
+				buffer.push(pairDouble);
+				departureTime = (buffer.front()[1] / transmissionSpeed) + delayTime;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// Clearing the pair
+			pairDouble.pop_back();
+			pairDouble.pop_back();
+		}
+    }
+
+    else {
+        cerr << "File failed to open." << endl;
+		return -1;
+    }
+
+    inputFile.close();
+
+	qos54Mbps.packetLoss = ((double)qos54Mbps.packetsLost / (double)packets) * 100.0;
+	qos54Mbps.averageDelay = qos54Mbps.delay / (double)packets;
+
+
+	transmissionSpeed = 3750000.0;
+	packets = 0;
+	currentTime = START;
+	inputFile.open("soccer.txt");
+
+    if (inputFile.is_open()) {
+        while (getline(inputFile, line)) {
+            packets++;   // Incrementing to count total number of packets
+            pairStr = stringSplit(line, ' ');
+			pairDouble.push_back(stod(pairStr[0]));
+			pairDouble.push_back(stod(pairStr[1]));
+
+			// If a packet arrives before the current one is finished transmitting
+			// add it to the queue
+			if (pairDouble[0] < currentTime) {
+				delayTime = currentTime - pairDouble[0];
+				qos30Mbps.delay += delayTime;
+			}
+
+			// When the buffer is full, it drops the packet and icrements the
+			// dropped packets counter
+			if (buffer.size() == maxBufferSize) {
+				// Drop packet
+				qos30Mbps.packetsLost++;
+			}
+
+			// When the buffer's size is 0, there will be no queuing delay as
+			// it is being transmitted immediately
+			else if (buffer.size() == 0) {
+				buffer.push(pairDouble);
+				delayTime = 0.0;
+				departureTime = buffer.front()[1] / transmissionSpeed;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// This case checks to see if the buffer is occupied by packets
+			// and applies a queuing delay when the packet is finally transmitted
+			else if (buffer.size() > 0 || buffer.size() < 100) {
+				buffer.push(pairDouble);
+				departureTime = (buffer.front()[1] / transmissionSpeed) + delayTime;
+				currentTime += departureTime;
+				buffer.pop();
+			}
+
+			// Clearing the pair
+			pairDouble.pop_back();
+			pairDouble.pop_back();
+		}
+    }
+
+    else {
+        cerr << "File failed to open." << endl;
+		return -1;
+    }
+
+    inputFile.close();
+
+	qos30Mbps.packetLoss = ((double)qos30Mbps.packetsLost / (double)packets) * 100.0;
+	qos30Mbps.averageDelay = qos30Mbps.delay / (double)packets;
+
+
+	cout << "|Speed in Mbps|\t|Average packet loss|\t|Average queuing delay|" << endl;
+	cout << " 11\t\t " << qos11Mbps.packetLoss << "%\t\t\t " << qos11Mbps.averageDelay * 1000 << " ms" << endl;
+	cout << " 6\t\t " << qos6Mbps.packetLoss << "%\t\t\t " << qos6Mbps.averageDelay * 1000 << " ms"  <<endl;
+	cout << " 54\t\t " << qos54Mbps.packetLoss << "%\t\t\t " << qos54Mbps.averageDelay * 1000 << " ms" << endl;
+	cout << " 30\t\t " << qos30Mbps.packetLoss << "%\t\t\t " << qos30Mbps.averageDelay * 1000 << " ms" << endl;
+
+
     return 0;
 }
